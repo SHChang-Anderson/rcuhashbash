8d145f45b6ecbc176ef0120f5f2a6a0a605feb6b
shchang <shchang.anderson@gmail.com>
2024-06-25 00:18:14 +0800

    Update test script

--- do_measurement.sh
+++ do_measurement.sh
@@ -1,71 +1,48 @@
+#!/bin/bash
+TARGET_MODULE=rcuhashbash-resize
+sleep_time=10
 tests=("rcu" "ddds" "rwlock")
 readers=(1 2 4 8 16)
-sleep_time=10
-TARGET_MODULE=rcuhashbash-resize
 
-sudo rmmod ${TARGET_MODULE} || true >/dev/null
-
-# test 1: no resizing and 2^13 buckets
 sudo dmesg -C
-for((tcount = 1; tcount <= 10; tcount = tcount + 1))
-do
-    # iterate algorithm
-    for((tests_count = 0; tests_count < 3; tests_count = tests_count + 1))
-    do
-        for((readers_count = 0; readers_count < 5; readers_count = readers_count + 1))
-        do
-            sudo insmod ${TARGET_MODULE}.ko \
-                    test=${tests[tests_count]} \
-                    readers=${readers[readers_count]} \
-                    shift1=13 \
-                    resize=false
-            sleep ${sleep_time}
-            sudo rmmod ${TARGET_MODULE}
-        done
-    done
-    echo test1 ${tcount} test done
-done
-sudo dmesg > test1result.txt
 
-# test 2: no resizing and 2^14 buckets
-sudo dmesg -C
-for((tcount = 1; tcount <= 10; tcount = tcount + 1))
-do
-    # iterate algorithm
-    for((tests_count = 0; tests_count < 3; tests_count = tests_count + 1))
-    do
-        for((readers_count = 0; readers_count < 5; readers_count = readers_count + 1))
-        do
-            sudo insmod ${TARGET_MODULE}.ko \
-                    test=${tests[tests_count]} \
-                    readers=${readers[readers_count]} \
-                    shift1=14 \
-                    resize=false
-            sleep ${sleep_time}
-            sudo rmmod ${TARGET_MODULE}
-        done
-    done
-    echo test2 ${tcount} test done
+for((readers_count = 0; readers_count < 5; readers_count = readers_count + 1))
+do    
+    sudo insmod ${TARGET_MODULE}.ko \
+            test=${tests[0]} \
+            readers=${readers[readers_count]} \
+            shift1=13 \
+            resize=false
+    sleep ${sleep_time}
+    sudo rmmod ${TARGET_MODULE}
 done
-sudo dmesg > test2result.txt
 
-# test 3: continuous resizing between $2^{13}$ and $2^{14}$ buckets
+sudo dmesg | grep "rcuhashbash summary" | grep -oP "reads: \K\d+(?= primary hits)" > 8k_buckets_without_resizing_rcu.txt
+
 sudo dmesg -C
-for((tcount = 1; tcount <= 10; tcount = tcount + 1))
-do
-    # iterate algorithm
-    for((tests_count = 0; tests_count < 3; tests_count = tests_count + 1))
-    do
-        for((readers_count = 0; readers_count < 5; readers_count = readers_count + 1))
-        do
-            sudo insmod ${TARGET_MODULE}.ko \
-                    test=${tests[tests_count]} \
-                    readers=${readers[readers_count]} \
-                    resize=true
-            sleep ${sleep_time}
-            sudo rmmod ${TARGET_MODULE}
-        done
-    done
-    echo test3 ${tcount} test done
+
+for((readers_count = 0; readers_count < 5; readers_count = readers_count + 1))
+do    
+    sudo insmod ${TARGET_MODULE}.ko \
+            test=${tests[1]} \
+            readers=${readers[readers_count]} \
+            shift1=13 \
+            resize=false
+    sleep ${sleep_time}
+    sudo rmmod ${TARGET_MODULE}
 done
-sudo dmesg > test3result.txt
+sudo dmesg | grep "rcuhashbash summary" | grep -oP "reads: \K\d+(?= primary hits)" > 8k_buckets_without_resizing_ddds.txt
+
+sudo dmesg -C
+
+for((readers_count = 0; readers_count < 5; readers_count = readers_count + 1))
+do    
+    sudo insmod ${TARGET_MODULE}.ko \
+            test=${tests[2]} \
+            readers=${readers[readers_count]} \
+            shift1=13 \
+            resize=false
+    sleep ${sleep_time}
+    sudo rmmod ${TARGET_MODULE}
+done
+sudo dmesg | grep "rcuhashbash summary" | grep -oP "reads: \K\d+(?= primary hits)" > 8k_buckets_without_resizing_rwlock.txt
--- script.gp
+++ script.gp
@@ -1,13 +1,13 @@
-set title "Fixed hashtable size v.s resize between two sizes"
+set title "8k buckets without resizing"
 set xlabel "Reader threads"
-set ylabel "Lookups/second (millions)"
+set ylabel "Lookups/second"
 set terminal png font "Times_New_Roman, 12"
 set key right
 
-set output "8k_16k_rwlock_resize.png"
+set output "8k_without_resize.png"
 
 
 plot \
-"8k_16k_rwlock_resize.txt" using 1:2 with linespoints linewidth 2 title "8k (fixed)", \
-"8k_16k_rwlock_resize.txt" using 1:3 with linespoints linewidth 2 title "16k (fixed)", \
-"8k_16k_rwlock_resize.txt" using 1:4 with linespoints linewidth 2 title "resize (rwlock)"\
+"8k_buckets_without_resizing_rcu.txt" using 1:2 with linespoints linewidth 2 title "rcu", \
+"8k_buckets_without_resizing_ddds.txt" using 1:2 with linespoints linewidth 2 title "ddds", \
+"8k_buckets_without_resizing_rwlock.txt" using 1:2 with linespoints linewidth 2 title "rwlock", \
